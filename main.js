3b0729ea11e9a9bfde59d382079e9e5c2547d265
Dory <148329667+kalibear99@users.noreply.github.com>
2024-01-05 09:38:30 +0100

    Create main.js

---
+++ main.js
@@ -0,0 +1,19 @@
+function Odmocnina(cislo) {
+  return Math.sqrt(cislo);
+}
+
+let vysledek = Odmocnina(25);
+console.log(vysledek);
+
+(lichost) = (a) => a % 2 == 1 ? "čislo je liché" : "číslo není liché"
+console.log(lichost(3))
+
+let a = 2;
+let b = 4;
+let c = 6;
+let d = 8;
+let e = 10;
+
+let prumer = (a + b + c + d + e) / 5;
+
+console.log("Průměr čísel je: " + prumer);
